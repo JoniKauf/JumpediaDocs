9e8004f4551b9b8d02027f32aaadcff5c6f2d508
JoniKauf <jonikaufdiscord@gmail.com>
2026-06-30 19:13:06 +0200

    Basic test structure

---
+++ docs/index.md
@@ -0,0 +1,14 @@
+# JUMPEDIA DOCS!!!
+
+<div style="padding:1em;border:1px solid black">
+This is raw HTML.
+</div>
+
+This is a first test. If you see this, then you are one of the first to do so
+and this is gonna be filled soon!
+
+```python
+test: int = 10
+```
+
+Greetings, Joni :)
--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-const sidebar = document.getElementById("sidebar");
-const resizeThreshold = 10;
-let isResizing = false;
-let startX = 0;
-let startWidth = 0;
+fetch("docs/index.md")
+    .then(r => r.text())
+    .then(md => {
+        const reader = new commonmark.Parser();
+        const writer = new commonmark.HtmlRenderer({
+            safe: false
+        });
 
-sidebar.addEventListener("mousemove", (e) => {
-    const rect = sidebar.getBoundingClientRect();
-    const nearRightEdge = rect.right - e.clientX < resizeThreshold;
-    sidebar.style.cursor = nearRightEdge ? "ew-resize" : "default";
-});
-
-sidebar.addEventListener("mousedown", (e) => {
-    const rect = sidebar.getBoundingClientRect();
-    if (rect.right - e.clientX < resizeThreshold) {
-    isResizing = true;
-    startX = e.clientX;
-    startWidth = rect.width;
-    document.body.style.cursor = "ew-resize";
-    e.preventDefault();
-    }
-});
-
-document.addEventListener("mousemove", (e) => {
-    if (!isResizing) return;
-    const dx = e.clientX - startX;
-    const newWidth = Math.min(Math.max(startWidth + dx, 100), 600); // clamp width
-    sidebar.style.width = `${newWidth}px`;
-});
-
-document.addEventListener("mouseup", () => {
-    if (isResizing) {
-    isResizing = false;
-    document.body.style.cursor = "default";
-    }
-});
+        const parsed = reader.parse(md);
+        document.getElementById("content").innerHTML = writer.render(parsed);
+    });
